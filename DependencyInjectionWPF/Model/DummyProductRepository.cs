e2e88590a24441ecbb7c8990060ceec807b01db0
Stino <Linksonder@gmail.com>
2016-11-18 14:48:17 +0100

    Add interface and dummy repo

---
+++ DependencyInjectionWPF/Model/DummyProductRepository.cs
@@ -0,0 +1,29 @@
+﻿using DependencyInjectionWPF.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DependencyInjectionWPF.Model
+{
+    public interface IProductRepository
+    {
+        List<ProductVM> GetAll();
+    }
+
+    class DummyProductRepository : IProductRepository
+    {
+        public List<ProductVM> GetAll()
+        {
+            return new List<ProductVM>()
+           {
+               new ProductVM() { Name = "6 Eieren", Price = 1.80 },
+               new ProductVM() { Name = "Melk", Price = 0.80 },
+               new ProductVM() { Name = "Oud brood", Price = 0 },
+               new ProductVM() { Name = "Bacon", Price = 2.40 },
+               new ProductVM() { Name = "Vaseline: XL", Price = 8  }
+           };
+        }
+    }
+}
